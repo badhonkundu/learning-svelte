7388963b77a011c3b66592e0f85e5f6861cd2c6b
badhon kundu <badhonkundu@gmail.com>
2020-11-04 08:20:34 +0530

    adding the bootstrap. Added a reactive variable

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	export let country = "India";
+
+	$: uppercaseName = name.toUpperCase();
+
+	const changeNameHandler = () => {
+		name ="Badhon Kundu";
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello I am {uppercaseName}, and I am from {country}!</h1>
+<button on:click="{changeNameHandler}"> Get full name</button>
+<input bind:value="{name}" />
