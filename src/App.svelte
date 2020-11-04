cb5e8afb48bb55f02eac7c0f2adef2cac3238db1
badhon kundu <badhonkundu@gmail.com>
2020-11-04 09:05:46 +0530

    added contact card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
+	import ContactCard from "./ContactCard.svelte";
 	export let name;
 	export let country = "India";
+
+	let jobTitle = "";
+	let description = "";
+	let imgUrl = "";
 
 	$: uppercaseName = name.toUpperCase();
 
 	const changeNameHandler = () => {
-		name ="Badhon Kundu";
-	}
+		name = "Badhon Kundu";
+	};
 </script>
 
 <style>
@@ -16,5 +21,14 @@
 </style>
 
 <h1>Hello I am {uppercaseName}, and I am from {country}!</h1>
-<button on:click="{changeNameHandler}"> Get full name</button>
-<input bind:value="{name}" />
+<button on:click={changeNameHandler}> Get full name</button>
+<input bind:value={name} />
+<input bind:value={jobTitle} />
+<input bind:value={imgUrl} />
+<textarea bind:value={description} />
+
+<!--
+sending props to contactcard componnet
+{description} and {jobTitle} are examples of self extending prop 
+-->
+<ContactCard userName={name} {jobTitle} {description} {imgUrl} />
